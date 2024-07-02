ecb6fdd9fd274f96c4950f229d6fe568780fb51a
ayman <yaoiotakusama@gmail.com>
2024-07-02 11:24:00 +0530

    chore: skip tests in service.spec.ts

--- packages/orderbook/src/lib/pws/service.spec.ts
+++ packages/orderbook/src/lib/pws/service.spec.ts
@@ -1,29 +1,29 @@
-import { expect, test, describe } from "vitest";
-import { PaymentChannelService } from "./service";
-import { JsonRpcProvider, Wallet, sha256 } from "ethers";
-import { Url } from "../url";
-import { Siwe } from "../auth/siwe";
-import { ERC20__factory } from "@catalogfi/wallets";
+import { expect, test, describe } from 'vitest';
+import { PaymentChannelService } from './service';
+import { JsonRpcProvider, Wallet, sha256 } from 'ethers';
+import { Url } from '../url';
+import { Siwe } from '../auth/siwe';
+import { ERC20__factory } from '@catalogfi/wallets';
 
 // this test is just a sanity check
 // test out the service in whatever way you see fit and skip it
 
-describe.skip("Payment channel service", () => {
-  // const pk = Wallet.createRandom().privateKey;
-  const pk = "...";
-  const provider = new JsonRpcProvider("...");
+describe.skip('Payment channel service', () => {
+  const pk = Wallet.createRandom().privateKey;
+  //   const pk = '...';
+  const provider = new JsonRpcProvider('...');
   const signer = new Wallet(pk, provider);
   const service = PaymentChannelService.init(
-    "https://feehubdev.garden.finance",
+    'https://feehubdev.garden.finance',
     signer,
-    new Siwe(new Url("https://stg-test-orderbook.onrender.com"), signer)
+    new Siwe(new Url('https://stg-test-orderbook.onrender.com'), signer)
   );
   const erc20 = ERC20__factory.connect(
-    "0x5eedb3f5bbA7Da86b0bBa2c6450C52E27e105eeD",
+    '0x5eedb3f5bbA7Da86b0bBa2c6450C52E27e105eeD',
     signer
   );
-  test.skip("create channel", async () => {
-    const res = await service.createChannel("1000000");
+  test.skip('create channel', async () => {
+    const res = await service.createChannel('1000000');
     expect(res.ok).toBeTruthy();
     const paymentChannelAddress = res.val.address;
     expect(paymentChannelAddress).toBeDefined();
@@ -33,7 +33,7 @@ describe.skip("Payment channel service", () => {
     });
   });
 
-  test.skip("get channel state", async () => {
+  test.skip('get channel state', async () => {
     const res = await service.getChannel();
     expect(res.ok).toBeTruthy();
     console.log(res.val);
@@ -41,11 +41,11 @@ describe.skip("Payment channel service", () => {
     // console.log(tx);
   });
 
-  test("get signature for conditional payment", async () => {
-    const secretHash = sha256(Buffer.from("0"));
+  test.skip('get signature for conditional payment', async () => {
+    const secretHash = sha256(Buffer.from('0'));
     const res = await service.createConditionalPayment({
-      sendAmount: "0",
-      receiveAmount: "1000",
+      sendAmount: '0',
+      receiveAmount: '1000',
       secretHash: secretHash,
     });
     expect(res.ok).toBeTruthy();
@@ -53,8 +53,8 @@ describe.skip("Payment channel service", () => {
     expect(res.error).toBeUndefined();
 
     const res2 = await service.payConditionally({
-      sendAmount: "0",
-      receiveAmount: "1000",
+      sendAmount: '0',
+      receiveAmount: '1000',
       secretHash: secretHash,
     });
     expect(res2.ok).toBeTruthy();
