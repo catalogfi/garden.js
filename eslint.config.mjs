8808b008d7fa427d03e2b2b0b541652c2075dd9a
ayman <yaoiotakusama@gmail.com>
2024-07-03 11:31:59 +0530

    chore: disable eslint no explicit any

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,7 +9,7 @@ export default [
   ...tseslint.configs.recommended,
   {
     rules: {
-      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-explicit-any': 'off',
     },
   },
 ];
