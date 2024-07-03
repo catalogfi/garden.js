8b2cbc29158ef1dda0b21329d73776c69b091472
ayman <yaoiotakusama@gmail.com>
2024-07-03 11:50:55 +0530

    chore: eslint fixes

--- packages/core/src/lib/test/testUtils.ts
+++ packages/core/src/lib/test/testUtils.ts
@@ -3,7 +3,7 @@ import { exec } from 'child_process';
 export const fund = async (address: string) => {
   exec(`merry faucet --to ${address}`);
 
-  await new Promise((resolve, reject) => {
+  await new Promise((resolve) => {
     setTimeout(() => {
       resolve(true);
     }, 5000);
