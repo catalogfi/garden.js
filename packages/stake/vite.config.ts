c3a4ae447fbf9ba55060e8c4f5d901faa3a45079
ManiReddyt <tmanireddy2001@gmail.com>
2024-07-15 13:20:01 +0530

    chore: Update package name to "@gardenfi/stake"

--- packages/stake/vite.config.ts
+++ packages/stake/vite.config.ts
@@ -4,7 +4,7 @@ import dts from 'vite-plugin-dts';
 import pkg from './package.json';
 
 export default defineConfig({
-  cacheDir: '../node_modules/.vite/orderbook',
+  cacheDir: '../node_modules/.vite/stake',
 
   plugins: [
     // nodePolyfills(),
@@ -28,7 +28,7 @@ export default defineConfig({
     lib: {
       // Could also be a dictionary or array of multiple entry points.
       entry: 'src/index.ts',
-      name: 'orderbook',
+      name: 'stake',
       fileName: 'index',
       // Change this to the formats you want to support.
       // Don't forget to update your package.json as well.
