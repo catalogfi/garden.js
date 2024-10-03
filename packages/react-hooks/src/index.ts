aba8d6f3c8e32e05b58353517887f2623255c61c
ManiReddyt <tmanireddy2001@gmail.com>
2024-10-03 22:51:57 +0530

    setup

---
+++ packages/react-hooks/src/index.ts
@@ -0,0 +1,5 @@
+export const Hello = () => {
+  console.log('Hello');
+};
+
+export { TestReact } from './lib/test';
---
+++ packages/react-hooks/src/lib/test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const TestReact = () => {
+  return (
+    <div>
+      <h1>React Test</h1>
+      <p>Testing React</p>
+    </div>
+  );
+};
