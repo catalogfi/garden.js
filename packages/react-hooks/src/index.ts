c5b5e2ee7218e1478a08dd8c5954880cde94e735
ManiReddyt <tmanireddy2001@gmail.com>
2024-11-11 22:05:43 +0700

    export garden configuration

--- packages/react-hooks/src/index.ts
+++ packages/react-hooks/src/index.ts
@@ -2,3 +2,4 @@ export { GardenProvider, useGarden } from './lib/context/GardenProvider';
 export { useOrderbook } from './lib/hooks/useOrderbook';
 export { useSecretManager } from './lib/hooks/useSecretManager';
 export { BitcoinNetwork } from '@catalogfi/wallets';
+export { environment, GARDEN_CONFIG } from './lib/gardenConfig';
