da34225abae335e6cb738f04b3800f563dab20f0
ManiReddyt <tmanireddy2001@gmail.com>
2025-05-09 12:19:45 +0530

    fix/clear unnecessary api calls in react-hooks

--- packages/react-hooks/src/lib/hooks/useOrderbook.tsx
+++ packages/react-hooks/src/lib/hooks/useOrderbook.tsx
@@ -1,16 +1,7 @@
-import {
-  filterDeadlineExpiredOrders,
-  IGardenJS,
-  OrderWithStatus,
-  ParseOrderStatus,
-} from '@gardenfi/core';
+import { IGardenJS, OrderWithStatus } from '@gardenfi/core';
 import { useEffect, useState } from 'react';
-import { DigestKey } from '@gardenfi/utils';
 
-export const useOrderbook = (
-  garden: IGardenJS | undefined,
-  digestKey: DigestKey | undefined,
-) => {
+export const useOrderbook = (garden: IGardenJS | undefined) => {
   const [pendingOrders, setPendingOrders] = useState<OrderWithStatus[]>([]);
 
   useEffect(() => {
@@ -31,41 +22,6 @@ export const useOrderbook = (
       garden.off('onPendingOrdersChanged', handlePendingOrdersChange);
     };
   }, [garden]);
-
-  // Fetch orders for the first time
-  useEffect(() => {
-    if (!garden || !digestKey) return;
-    garden.blockNumberFetcher.fetchBlockNumbers().then((res) => {
-      if (res.error) return;
-      const { val: blockNumbers } = res;
-      garden.orderbook
-        .getMatchedOrders(digestKey.userId, 'pending', {
-          per_page: 500,
-        })
-        .then((orders) => {
-          if (orders.error) return;
-          const orderWithStatus = filterDeadlineExpiredOrders(orders.val.data)
-            .map((order) => {
-              const sourceBlockNumber = blockNumbers[order.source_swap.chain];
-              const destinationBlockNumber =
-                blockNumbers[order.destination_swap.chain];
-              if (!sourceBlockNumber || !destinationBlockNumber) return;
-              const status = ParseOrderStatus(
-                order,
-                sourceBlockNumber,
-                destinationBlockNumber,
-              );
-              return {
-                ...order,
-                status,
-              };
-            })
-            .filter((order) => order !== undefined);
-
-          setPendingOrders(orderWithStatus);
-        });
-    });
-  }, [garden, digestKey]);
 
   return { pendingOrders };
 };
--- packages/test/src/App.tsx
+++ packages/test/src/App.tsx
@@ -1,15 +1,12 @@
 import { GardenProvider } from '@gardenfi/react-hooks';
-import { DigestKey, Environment, Siwe, Url } from '@gardenfi/utils';
+import { Environment } from '@gardenfi/utils';
 import { useWalletClient } from 'wagmi';
 import { Swap } from './components/Swap';
 import { useAccount } from '@starknet-react/core';
-import { EvmRelay, Quote, StarknetRelay } from '@gardenfi/core';
-import { Orderbook } from '@gardenfi/orderbook';
 
 function App() {
   const { data: walletClient } = useWalletClient();
   const { account: starknetAccount } = useAccount();
-  console.log('walletClient :', walletClient);
 
   return (
     <GardenProvider
@@ -18,26 +15,10 @@ function App() {
           environment: Environment.TESTNET,
           orderbook: 'https://testnet.api.hashira.io',
         },
-        // wallets: {
-        //   evm: walletClient,
-        //   starknet: starknetWallet,
-        // },
-        htlc: {
-          // starknet: new StarknetRelay(
-          //   'https://starknet-relayer.hashira.io',
-          //   starknetAccount!,
-          // ),
-          evm: new EvmRelay(
-            'https://orderbook-stage.hashira.io',
-            walletClient!,
-            Siwe.fromDigestKey(
-              new Url('https://orderbook-stage.hashira.io'),
-              DigestKey.generateRandom().val!,
-            ),
-          ),
+        wallets: {
+          evm: walletClient,
+          starknet: starknetAccount,
         },
-        quote: new Quote('https://quote-staging.hashira.io/'),
-        // api: 'https://orderbook-stage.hashira.io',
       }}
     >
       <Swap />
