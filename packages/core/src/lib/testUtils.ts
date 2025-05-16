e8e559818879c1a291ad946ba661b6f5d38ed0ab
SATHVIK SVS <svssathvik77@gmail.com>
2025-05-16 14:23:22 +0530

    add tests for bitcoin wallets

--- packages/core/src/lib/testUtils.ts
+++ packages/core/src/lib/testUtils.ts
@@ -1,9 +1,12 @@
 import { exec } from 'child_process';
 import { Asset, Chains, Chain as Network } from '@gardenfi/orderbook';
-
+import * as bitcoin from 'bitcoinjs-lib';
+import * as ecc from 'tiny-secp256k1';
 import { Chain } from 'viem';
 import { SwapParams } from './garden/garden.types';
 import { Fetcher } from '@gardenfi/utils';
+import { IBitcoinProvider } from './bitcoin/provider/provider.interface';
+import ECPairFactory from 'ecpair';
 
 export const fund = async (address: string) => {
   exec(`merry faucet --to ${address}`);
@@ -137,4 +140,77 @@ export const createOrderObject = (
     minDestinationConfirmations: 3,
   };
   return order;
+};
+
+export class regTestUtils {
+  static async mine(blocks: number, provider: IBitcoinProvider) {
+    const block = await provider.getLatestTip();
+    exec(`merry rpc --generate ${blocks}`, (error, stdout, stderr) => {
+      if (error) {
+        throw error;
+      }
+      if (stderr) {
+        throw new Error(stderr);
+      }
+    });
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+      const newBlock = await provider.getLatestTip();
+      if (newBlock > block) {
+        break;
+      }
+      await new Promise((resolve) => setTimeout(resolve, 100));
+    }
+  }
+  /**
+   * funds the address with 1 BTC
+   */
+  static async fund(address: string, provider: IBitcoinProvider) {
+    const balance = await provider.getBalance(address);
+    exec(`merry faucet --to ${address}`, async (error, stdout, stderr) => {
+      if (error) {
+        throw error;
+      }
+      if (stderr) {
+        throw new Error(stderr);
+      }
+      await this.mine(10, provider);
+    });
+    while ((await provider.getBalance(address)) === balance) {
+      await new Promise((resolve) => setTimeout(resolve, 100));
+    }
+  }
+}
+
+export const getSignerFromPk = async (pk: string, network: bitcoin.Network) => {
+  return ECPairFactory(ecc).fromPrivateKey(Buffer.from(pk, 'hex'), {
+    network,
+  });
+};
+
+export const getScript = (network: bitcoin.Network, type: 'p2sh' | 'p2wsh') => {
+  const script = bitcoin.script.fromASM(
+    `
+        OP_ADD
+        ${bitcoin.script.number.encode(4).toString('hex')}
+        OP_EQUALVERIFY
+        OP_CHECKSIG
+    `
+      .trim()
+      .replace(/\s+/g, ' '),
+  );
+
+  const payment = bitcoin.payments[type]({
+    redeem: {
+      output: script,
+    },
+    network,
+  });
+
+  if (!payment.address) throw new Error('Could not build address');
+
+  return {
+    script,
+    address: payment.address,
+  };
 };
